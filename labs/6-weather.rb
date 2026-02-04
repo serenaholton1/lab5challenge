279bf5298ab3e9176a85aafdbe0c18ada86c2583
serenaholton1 <serena.holton@kellogg.northwestern.edu>
2026-02-04 02:27:26 +0000

    finishing lab 6

--- labs/6-weather.rb
+++ labs/6-weather.rb
@@ -1,6 +1,7 @@
 # To run this code, be sure your current working directory
 # is the same as where this file is located and then run:
 # ruby 6-weather.rb
+
 # EXERCISE
 # Given the following Ruby hash that represents weather data,
 # write a weather summary out to the screen. Something like this:
@@ -15,6 +16,7 @@
 # - 60 degrees and Rainy
 # Note: only display the chance of rain if above 50%.
 # Note: do not display the chance of rain if conditions are already rainy.
+
 weather_data = {
   current: {
     temperature: 67,
@@ -30,3 +32,25 @@ weather_data = {
     { temperature: 60, conditions: "Rainy", precipitation: 0.9 }
   ]
 }
+
+
+current_temp = weather_data[:current][:temperature]
+current_cond = weather_data[:current][:conditions]
+
+puts "currently it is #{current_temp} degrees and #{current_cond}"
+puts "forecast for the next 7 days:"
+
+for day in weather_data[:forecast]
+  temp = day[:temperature]
+  conditions = day[:conditions]
+  precip = day[:precipitation]
+
+  display_string = "-#{temp} degrees and #{conditions}"
+
+  if precip >0.5 && conditions !="Rainy"
+    chance = (precip * 100).to_i
+    display_string = display_string + "with a #{chance}% of rain"
+  end
+
+  puts display_string
+end
